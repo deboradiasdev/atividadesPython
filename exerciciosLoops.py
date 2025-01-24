6d63194502618362e4454a76d5570b65e93e176a
deboradiasdev <contatodeboradev@gmail.com>
2025-01-24 20:17:54 -0300

    fim do exercicio

--- exerciciosLoops.py
+++ exerciciosLoops.py
@@ -5,5 +5,18 @@ for nome in nomes :
     print(nome)
 
 #Faça o mesmo exercício acima utilizando o loop while
-while len(nomes) > 0:
+while len(nomes) > 0 :
     print(nomes.pop(0))
+
+#Crie uma função que recebe a lista de nomes como parâmetro e passe por algum dos loops criados acima
+def imprimir_nomes(lista) :
+    for nome in lista :
+        print(nome)
+
+imprimir_nomes(nomes)
+
+#Utilize o loop do exercício 1 ou 2 e printe somente os nomes que começam com letra “A”
+
+for nome in nomes:
+    if nome[0] == 'A':
+        print(nome)
