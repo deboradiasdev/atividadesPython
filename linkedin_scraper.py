87abfe5741a5e473c97a03d47841c046d9f52afc
deboradiasdev <contatodeboradev@gmail.com>
2025-02-24 23:26:51 -0300

    scraper

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -1,30 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-driver.get("https://www.linkedin.com/login")
-
-time.sleep(30)
-
-url_vagas = "https://www.linkedin.com/jobs/search/?keywords=Cientista%20de%20Dados"
-driver.get(url_vagas)
-
-time.sleep(10)
-
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pandas as pd
 
-vagas = driver.find_elements(By.CLASS_NAME, "job-card-container")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.linkedin.com/")
 
-for vaga in vagas:
-    try:
-        titulo = vaga.find_element(By.CSS_SELECTOR, ".job-card-list__title").text
-        empresa = vaga.find_element(By.CSS_SELECTOR, ".job-card-container__company-name").text
-        localizacao = vaga.find_element(By.CSS_SELECTOR, ".job-card-container__metadata-item").text
+vagas_botao = driver.find_element(By.XPATH, "/html/body/nav/ul/li[4]/a")
+vagas_botao.click()
 
-        print(f"Vaga: {titulo}\nEmpresa: {empresa}")
-    except Exception as e:
-        print(f"Erro ao extrair informações da vaga: {e}")
+try:
+    pop_up = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section/button'))
+    )
+    pop_up.click()
+except Exception as e:
+    print("Pop-up não encontrado ou erro ao fechar:", e)
+
+vaga_titulo = driver.find_element(By.XPATH, '//*[@id="job-search-bar-keywords"]')
+vaga_titulo.click()
+
+vaga_titulo.send_keys("Analista de Dados")
+
+vaga_localizacao = driver.find_element(By.XPATH, '//*[@id="job-search-bar-location"]')
+vaga_localizacao.click()
+vaga_localizacao.clear()
+
+vaga_localizacao.send_keys("Brasil")
+vaga_localizacao.send_keys(Keys.ARROW_DOWN)
+vaga_localizacao.send_keys(Keys.RETURN)
+
+vaga_card = driver.find_elements(By.CLASS_NAME, 'base-card__full-link')
+len(vaga_card)
+vaga_card[1].click()
+
+vaga_textos = [top-card-layout__title for job in vaga_card]
+vaga_textos
